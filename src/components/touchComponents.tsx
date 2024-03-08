3e0ebb971e936a854e74fe659c0aa24a3dcb10e3
KaueD <kauedomingueso98@gmail.com>
2024-03-08 16:38:40 -0300

    Gerando aleatorio pontos

--- src/components/touchComponents.tsx
+++ src/components/touchComponents.tsx
@@ -1,7 +1,24 @@
-import { Flex } from "@chakra-ui/react"
+import { Flex, Text } from "@chakra-ui/react"
 import { TouchPoint } from "./touchPoint"
+import { useEffect, useState } from "react"
 
-export const TouchComponents = () =>{
+export const TouchComponents = () => {
+
+  const [positionX, setPositionX] = useState<string>();
+  const [positionY, setPositionY] = useState<string>();
+  const [next, setNext] = useState<number>(0);
+
+  const randomInt = (min: number, max: number): number => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  };
+  
+
+  useEffect(() => {
+    setPositionX(`${randomInt(0, 90)}%`);
+    setPositionY(`${randomInt(0, 90)}%`);
+  }, [next]);
 
   return (
     <Flex
@@ -10,7 +27,8 @@ export const TouchComponents = () =>{
       position={'absolute'}
       zIndex={'10'}
     >
-      <TouchPoint />
+      <Text color={"yellow"}>{next}</Text>
+      <TouchPoint key={next} onError={() => {setNext((ant)=>ant!-1) }} onSucess={() => {setNext((ant)=>ant!+1) }} x={positionX!} y={positionY!} />
     </Flex>
   )
 }
--- src/components/touchPoint.tsx
+++ src/components/touchPoint.tsx
@@ -1,48 +1,62 @@
 import { Flex } from "@chakra-ui/react"
 import { useEffect, useState } from "react"
 
-export const TouchPoint = () =>{
+export interface TouchPointContent {
+  x: string,
+  y: string,
+  onError: () => void
+  onSucess: () => void
+}
+
+export const TouchPoint = ({ x, y, onSucess, onError }: TouchPointContent) => {
 
   const [scale, setScale] = useState(0.1);
   const [status, setStatus] = useState('pedding');
-  const [animationInterval, setAnimationInterval] = useState<undefined|number>();
+  const [animationInterval, setAnimationInterval] = useState<undefined | number>();
 
   const onTouch = () => {
-    if(animationInterval){
+    if (animationInterval) {
       clearInterval(animationInterval);
     }
 
     setStatus('sucess');
+    onSucess();
   };
-  
-  useEffect(()=>{
-    if(animationInterval === undefined){
-      setAnimationInterval(setInterval(()=>{
-        setScale((ant)=>{
+
+  useEffect(() => {
+
+    if (animationInterval === undefined && status === 'pedding') {
+      setAnimationInterval(setInterval(() => {
+        setScale((ant) => {
           const nextScale = ant + 0.1;
-          
-          if(nextScale >= 1.2){
+
+          if (nextScale >= 1.2) {
             clearInterval(animationInterval);
             setStatus('error');
           }
           
-          return nextScale >= 1.2 ? 1.2 : nextScale;
+          return nextScale;
         });
-      }, 100));
-    } 
-  }, [animationInterval]);
+      }, 120));
+    } else if (status !== 'pedding' && animationInterval) {
+      clearInterval(animationInterval);
+      onError();
+    }
+
+  }, [animationInterval, status, onError]);
 
   return (
     <Flex
       w={`${50 * scale}px`}
       h={`${50 * scale}px`}
       position={'absolute'}
-      top={'50%'}
-      right={'50%'}
-      onClick={status === 'pedding' ? onTouch : ()=>{}}
-      bg={ status == 'error' ? 'red': 'white'}
+      top={y}
+      right={x}
+      onClick={status === 'pedding' ? onTouch : () => { }}
+      bg={status == 'error' ? 'red' : 'white'}
+      cursor={'pointer'}
     >
-      
+
     </Flex>
   )
 }
